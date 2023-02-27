2de0178c9a8217d934ad1d544d8340dc06f1cd17
mug3njutsu <johngitahi272@gmail.com>
2023-02-27 14:49:45 +0300

    Task 1

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers
+If invalid, returns 0
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Function that queries the Reddit API
+    - If not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if req.status_code == 200:
+        return req.json().get("data").get("subscribers")
+    else:
+        return 0
